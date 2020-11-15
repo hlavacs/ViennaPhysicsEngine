46c883ff48ea2ab47de42934b39fd3b6fbaa59c8
Helmut Hlavacs <helmut.hlavacsq@univie.ac.at>
2020-11-15 11:50:20 +0100

    improved sat

--- main.cpp
+++ main.cpp
@@ -16,22 +16,22 @@
 int main() {
 
 	Box ground( vec3( 0.0f, -50.0f, 0.0f), scale( mat4(1.0f), vec3(100.0f, 100.0f, 100.0f)) );
-	Box box( vec3( 0.0f, 1.0f, 0.0f ) ) ;
-	vec3 mtv(0,0,0); //minimum translation vector
-	auto hit = gjk(&box, &ground, &mtv);
+	Box box( vec3( 0.0f, 0.500001f, 0.0f ) ) ;
+	vec3 mtv(0,1,0); //minimum translation vector
+	auto hit1 = gjk(&box, &ground, &mtv);
 
-	hit = sat( &box, &ground, &mtv);
+	auto hit2 = sat( (Collider*)&box, (Collider*)&ground, &mtv);
 
 	Point point( vec3(0,-10,0) );
-	hit = gjk(&point, &ground, &mtv);
+	auto hit3 = gjk(&point, &ground, &mtv);
 
 	Triangle triangle( vec3(0.0, -0.0, 0.0), rotate( mat4(1.0f), (float)M_PI_2, vec3(1.0f, 0.0f, 0.0f) ) );
-	hit = gjk(&triangle, &ground, &mtv);
+	auto hit4 = gjk(&triangle, &ground, &mtv);
 
 	Quad quad( vec3(0.0, -0.01, 0.0) );
-	hit = gjk(&quad, &ground, &mtv);
+	auto hit5 = gjk(&quad, &ground, &mtv);
 
-	hit = gjk(&triangle, &quad, &mtv);
+	auto hit7 = gjk(&triangle, &quad, &mtv);
 
 
 
--- sat.h
+++ sat.h
@@ -7,35 +7,74 @@
 
 #include "Collider.h"
 #include "hash.h"
+#include <random>
 
 
 using v3pair = std::pair<vec3,vec3>;
 
 
-bool sat_axis_test( Collider *obj1, Collider *obj2, vec3 *dir, vec3 *r) {
+bool sat_axis_test( Collider *obj1, Collider *obj2, vec3 *dir, vec3 *r, float *d) {
     vec3 p = obj1->support( *dir );
     vec3 q = obj2->support( -1.0f* (*dir) );
     *r = normalize(q - p); 
-    return dot( *r, *dir ) > 0.0f;
+    *d = dot( *r, *dir );
+    return  *d > 0.0f;
+
+}
+
+constexpr int NUM_RANDOM_DIR = 64;
+
+bool sat_random_test(Collider *obj1, Collider *obj2, vec3 *dir) {
+    static std::vector<vec3> random_axes;   //Fibonacci sphere
+    if( random_axes.empty()) {
+        float phi = (float)(M_PI * (3. - std::sqrt(5.)));  // golden angle in radians
+
+        for(int i=0; i<NUM_RANDOM_DIR; ++i ) {
+            float y = 1 - (i / float(NUM_RANDOM_DIR - 1)) * 2;  // y goes from 1 to -1
+            float radius = std::sqrt(1 - y * y);         // radius at y
+
+            float theta = phi * i;                       // golden angle increment
+
+            float x = std::cos(theta) * radius;
+            float z = std::sin(theta) * radius;
+            random_axes.emplace_back( x, y, z );
+        }
+    }
+
+    vec3 r;
+    float d;
+    float max = -1.0e6;
+    bool found = false;
+    for( int i=0; i<random_axes.size() && !found; ++i ) {        
+        found = sat_axis_test(obj1, obj2, &random_axes[i], &r, &d);
+        if( d>max ) { 
+            max = d;
+            *dir = random_axes[i];
+        }
+    }
+    return found;
 }
 
 bool sat_chung_wang_test( Collider *obj1, Collider *obj2, vec3 *dir, int max_loops = 100 ) {
     vec3 r;
+    float d;
     int loop = 0;
-    while( loop < max_loops && sat_axis_test(obj1, obj2, dir, &r) ) {
+    while( loop < max_loops && !sat_axis_test(obj1, obj2, dir, &r, &d) ) {
         *dir = reflect( *dir, r); //dir = dir - 2*dot(r, dir)*r
         ++loop;
     }
-    return sat_axis_test(obj1, obj2, dir, &r);
+    std::cout << "Loops: " << loop << std::endl;
+    return sat_axis_test(obj1, obj2, dir, &r, &d);
 }
 
 bool sat_faces_test( Polytope *obj1, Polytope *obj2, vec3 *dir ) {
     auto &faces = obj1->faces2;
     vec3 r;
+    float d;
     bool found = false;
     for( int i=0; !found && i<faces.size(); ++i) {
         *dir = obj1->get_normal_of_face(i);
-        found = sat_axis_test(obj1, obj2, dir, &r);
+        found = sat_axis_test(obj1, obj2, dir, &r, &d);
     }
     return found;
 }
@@ -48,11 +87,12 @@ bool sat_edges_test( Polytope *obj1, Polytope *obj2, vec3 *dir ) {
     obj2->get_edge_vectors( edges2 );
 
     vec3 r;
+    float d;
     bool found = false;
     for( int i=0; !found && i<edges1.size(); ++i) {
         for( int j=0; !found && i<edges2.size(); ++j) {
             *dir = cross( edges1[i], edges2[j] );
-            found = sat_axis_test(obj1, obj2, dir, &r);
+            found = sat_axis_test(obj1, obj2, dir, &r, &d);
         }
     }
     return found;
@@ -60,14 +100,23 @@ bool sat_edges_test( Polytope *obj1, Polytope *obj2, vec3 *dir ) {
 
 bool sat( Polytope *obj1, Polytope *obj2, vec3 *dir ) {
     if( dot(*dir, *dir) < 1.0e-6 ) *dir = vec3(0.0f, 1.0f, 0.0f);
-    bool found = sat_faces_test( obj1, obj2, dir );
-    if( !found ) found = sat_edges_test( obj1, obj2, dir );
-    return !found;
+    if( sat_faces_test( obj1, obj2, dir ) ) return false;
+
+    if( obj1->edges2.size() * obj2->edges2.size() > NUM_RANDOM_DIR ) {
+        if( sat_random_test( obj1, obj2, dir ) ) return false;
+        if( sat_edges_test( obj1, obj2, dir ) ) return false;
+    } else {
+        if( sat_edges_test( obj1, obj2, dir ) ) return false;
+        if( sat_random_test( obj1, obj2, dir ) ) return false;
+    }
+    return true;
 }
 
 bool sat( Collider *obj1, Collider *obj2, vec3 *dir ) {
     if( dot(*dir, *dir) < 1.0e-6 ) *dir = vec3(0.0f, 1.0f, 0.0f);
-    return !sat_chung_wang_test( obj1, obj2, dir );
+    if( sat_random_test( obj1, obj2, dir ) ) return false;
+    if( sat_chung_wang_test( obj1, obj2, dir ) ) return false;
+    return true;
 }
 
 
