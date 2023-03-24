927b850ec46101b75c80334403bb5898fe8a36cb
Felix-Neumann-AT <127500676+Felix-Neumann-AT@users.noreply.github.com>
2023-03-24 12:34:05 +0100

    Changed the soft body loading process so that normals (need to be calculated dynamically) and textures (for now) are not loaded.

--- VulkanEngine/VESceneManager.cpp
+++ VulkanEngine/VESceneManager.cpp
@@ -1330,10 +1330,15 @@ namespace ve
 		Assimp::Importer importer;
 		std::string filekey = basedir + "/" + filename;
 
+		// Ignore normals (need to be calculated dynamically)
+		// and textures (!for now!), since they lead to vertex duplication which makes the
+		// synchronization of the render body with the physics world body much harder
+		importer.SetPropertyInteger(AI_CONFIG_PP_RVC_FLAGS,
+			aiComponent_NORMALS | aiComponent_TEXCOORDS);
+
 		// Load the scene from the file
 		const aiScene* pScene = importer.ReadFile(filekey,
-			aiProcess_GenNormals | aiProcess_CalcTangentSpace |
-			aiProcess_Triangulate);
+			aiProcess_RemoveComponent | aiProcess_JoinIdenticalVertices);
 
 		// Check if the loaded scene is valid
 		if (!pScene)
@@ -1344,6 +1349,8 @@ namespace ve
 
 		// Get the model (first (and idealy only) child) from the scene
 		auto node = pScene->mRootNode->mChildren[FIRST_MESH_INDEX];
+
+		std::cout << pScene->mMeshes[FIRST_MESH_INDEX]->mNumVertices << std::endl;
 
 		// Create a SoftBodyMesh
 		VESoftBodyMesh* pSoftBodyMesh = new VESoftBodyMesh("Soft_Body_Mesh",
